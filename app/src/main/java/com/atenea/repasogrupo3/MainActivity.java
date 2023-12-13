e573acb043e6cb5257662157be76683045d65f16
ftec16ateneabog <ftec16atenea_bog@unal.edu.co>
2023-12-12 21:25:17 -0500

    first commit

--- app/src/main/java/com/atenea/repasogrupo3/MainActivity.java
+++ app/src/main/java/com/atenea/repasogrupo3/MainActivity.java
@@ -3,7 +3,12 @@ package com.atenea.repasogrupo3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.DatePicker;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +29,36 @@ public class MainActivity extends AppCompatActivity {
                 int a = 1;
             }
         });*/
+        TextInputEditText nombre = findViewById(R.id.NompreInputText);
+        nombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(Seguridad.isValidMail(charSequence.toString()))
+                {
+                    Toast.makeText(MainActivity.this, "pattern El correo es valido",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "pattern El correo no es valido",Toast.LENGTH_LONG).show();
+                }
+                if(Seguridad.isValidText(charSequence.toString()))
+                {
+                    Toast.makeText(MainActivity.this, "manual El correo es valido",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "manual El correo no es valido",Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/atenea/repasogrupo3/Seguridad.java
+++ app/src/main/java/com/atenea/repasogrupo3/Seguridad.java
@@ -1,5 +1,24 @@
 package com.atenea.repasogrupo3;
 
-public class Seguridad {
+import android.util.Patterns;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Seguridad {
+    public static boolean isValidNombre(String nombre,int min,int max) {
+        int numberChart = (((nombre).toString()).trim()).length();
+        return  numberChart >=  min && numberChart <= max;
+    }
+    public static boolean isValidTel(String tel)
+    {
+        return Patterns.PHONE.matcher(tel).matches();
+    }
+    public static boolean isValidMail(String mail)
+    {
+        return Patterns.EMAIL_ADDRESS.matcher(mail).matches();
+    }
+    public  static boolean isValidText(String text)
+    {
+        return text.matches("^[a-zA-Z0-9_+&*-]+(?:\\\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,7}$");
+    }
 }
